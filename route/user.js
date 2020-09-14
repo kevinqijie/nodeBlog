1be4e081cfdbe31ccd06320536a503e5891d1a00
七戒 <1123093510@qq.com>
2020-09-14 11:41:20 +0800

    c1

--- route/user.js
+++ route/user.js
@@ -1,14 +1,59 @@
 const express = require('express');
 const router = express.Router();
-const user  = require('../sqlDir/user');
+const user = require('../sqlDir/user');
 const bcrypt = require('bcrypt');
-router.post('/login',(req,res)=>{
-    const {title, create_user, content, category,description } = req.body
-    user.login(title, create_user, content,category, description, (key)=>{
-        res.json({msg:key})
+const jwt = require('jsonwebtoken');
+
+router.post('/login', (req, res) => {
+    const {email, password} = req.body
+    user.login(email, (row) => {
+        if(row==-1){
+            res.json({
+                msg: "0",
+                message: '登录失败！'
+            })
+            return false
+        }
+        if(row ==0){
+            res.json({
+                msg: "0",
+                message: '用户不存在！'
+            })
+            return false
+        }
+        //要检查密码 bcrypt
+        bcrypt.compare(password, row.password)
+            .then(isPasswrod => {
+                if (isPasswrod) {
+                    const rule = {
+                        id: row.id,
+                        username: row.username
+                    }
+                    // 规则 rule  加密名字 secret   过期时间expiresIn:3600  回调()=>{}
+                    jwt.sign(rule, 'secret', {expiresIn: 36000}, (err, token) => {
+                        if (err) throw err;
+                        res.json({
+                            msg: "1",
+                            token: "Bearer " + token,
+                            message: '登陆成功'
+                        })
+                    })
+
+                } else {
+                    return res.status(400).json({
+                        msg: '0',
+                        message: '密码错误！'
+                    })
+                }
+            })
     })
 })
-router.post('/register',(req,res)=>{
+
+
+/**
+ *
+ */
+router.post('/register', (req, res) => {
     const {email, username, password} = req.body
     //依赖模块 bcrypt
     bcrypt.genSalt(10, function (err, salt) {
@@ -20,7 +65,11 @@ router.post('/register',(req,res)=>{
                     message: '注册失败',
                     err
                 })
+                return false
             }
+            user.register(email, username, hash, (key) => {
+                res.json({msg: key})
+            })
         })
     })
     // user.register(title, create_user, content,category, description, (key)=>{
--- sqlDir/user.js
+++ sqlDir/user.js
@@ -1,20 +1,26 @@
+const {query} = require('../config/mysql')
 class User {
-    static login(category, page, callback) {
-        query(`SELECT * FROM article_list WHERE category =${category}`, (err, rows, fields) => {
+    static login(email, callback) {
+        query(`SELECT * FROM user WHERE email =${email}`, (err, rows, fields) => {
             if (err) {
-                console.log(err);
+                callback(-1);
+
+            }
+            if(rows.length == 0){
+                callback(0);
                 return;
             }
             callback(rows[0]);
         })
     }
-    static register(category, page, callback) {
-        query(`SELECT * FROM article_list WHERE category =${category}`, (err, rows, fields) => {
+    static register(email, username,password, callback) {
+        query(`INSERT INTO user (email, username,password) VALUES( '${email}', '${username}','${password}')`, (err, rows, fields) => {
             if (err) {
                 console.log(err);
+                callback('操作失败');
                 return;
             }
-            callback(rows[0]);
+            callback('操作成功');
         })
     }
 }
